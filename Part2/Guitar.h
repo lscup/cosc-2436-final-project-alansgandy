823d0a22b3308a43f1e1c1b68b72e906a1030cf0
Alan S Gandy <alan.s.gandy@lonestar.edu>
2023-12-06 11:46:16 -0600

    Update Guitar.h
    updated for part 2

--- Part2/Guitar.h
+++ Part2/Guitar.h
@@ -1,34 +1,32 @@
-#include <vector>
-#include <map>
-#include <string>
+class Guitar:
+    guitar_collection = []
+    guitar_map = {}
+    
+    def __init__(self, model, brand, year, type):
+        self.model = model
+        self.brand = brand
+        self.year = year
+        self.type = type
 
+    @classmethod
+    def add_guitar(cls, guitar):
+        cls.guitar_collection.append(guitar)
+        cls.guitar_map[guitar.model] = guitar
 
+    @classmethod
+    def remove_guitar(cls, model):
+        cls.guitar_collection = [g for g in cls.guitar_collection if g.model != model]
+        if model in cls.guitar_map:
+            del cls.guitar_map[model]
 
-class Guitar {
-private:
-    std::string model;
-    std::string brand;
-    int year;
-    std::string type;
+    @classmethod
+    def sort_by_year(cls):
+        cls.guitar_collection.sort(key=lambda g: g.year)
 
+    @classmethod
+    def search_by_model(cls, model):
+        return next((g for g in cls.guitar_collection if g.model == model), None)
 
-    static std::vector<Guitar> guitarCollection;
-    static std::map<std::string, Guitar> guitarMap;
-
-
-public:
-    Guitar(std::string model, std::string brand, int year, std::string type);
-
-
-    // Getters and Setters
-
-    std::string getModel() const;
-    int getYear() const;
-
-    static void addGuitar(const Guitar& guitar);
-    static void removeGuitar(const std::string& model);
-
-    static void sortByYear();
-    static Guitar* searchByModel(const std::string& model);
-    static Guitar* searchByModelInMap(const std::string& model);
-};
+    @classmethod
+    def search_by_model_in_map(cls, model):
+        return cls.guitar_map.get(model, None)
