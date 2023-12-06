667e11b2dc7b8823d03c051e9e4048fffd55a95a
Alan S Gandy <alan.s.gandy@lonestar.edu>
2023-12-06 11:44:28 -0600

    Update main.py

--- Part1/Guitar.h
+++ Part1/Guitar.h
@@ -1,34 +1,10 @@
-#include <vector>
-#include <map>
-#include <string>
-
-
-
-class Guitar {
-private:
-    std::string model;
-    std::string brand;
-    int year;
-    std::string type;
-
-
-    static std::vector<Guitar> guitarCollection;
-    static std::map<std::string, Guitar> guitarMap;
-
-
-public:
-    Guitar(std::string model, std::string brand, int year, std::string type);
-
-
-    // Getters and Setters
-
-    std::string getModel() const;
-    int getYear() const;
-
-    static void addGuitar(const Guitar& guitar);
-    static void removeGuitar(const std::string& model);
-
-    static void sortByYear();
-    static Guitar* searchByModel(const std::string& model);
-    static Guitar* searchByModelInMap(const std::string& model);
-};
+class Guitar:
+    guitar_collection = []
+    guitar_map = {}
+    
+    def __init__(self, model, brand, year, type):
+        self.model = model
+        self.brand = brand
+        self.year = year
+        self.type = type
+        
